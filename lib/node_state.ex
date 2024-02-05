39367a2f68dfe0f55cb0749e883c42520af4c8e6
Emil Vikström (iut482) <emil.vikstrom@tv4.se>
2024-02-05 15:03:28 +0100

    Add a state to NodeState

--- lib/node_state.ex
+++ lib/node_state.ex
@@ -2,24 +2,40 @@ defmodule BeamClusterCommon.NodeState do
   use GenServer
   require Logger
 
+  def get_state() do
+    GenServer.call(__MODULE__, :get_state)
+  end
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_) do
     :net_kernel.monitor_nodes(true)
-    {:ok, :ok}
+    {:ok, %{}}
+  end
+
+  def handle_call(:get_state, _, state) do
+    {:reply, state, state}
   end
 
   def handle_info({:nodeup, node_name}, state) do
-    Logger.info("Node up #{inspect(node_name)}")
+    service_name = get_service_name(node_name)
 
-    {:noreply, state}
+    nodes = Map.get(state, service_name, [])
+
+    {:noreply, Map.put(state, service_name, [node_name | nodes])}
   end
 
   def handle_info({:nodedown, node_name}, state) do
-    Logger.info("Node down #{inspect(node_name)}")
+    service_name = get_service_name(node_name)
+    nodes = Map.get(state, service_name, []) |> List.delete(node_name)
 
-    {:noreply, state}
+    {:noreply, Map.put(state, service_name, nodes)}
+  end
+
+  defp get_service_name(node_name) do
+    [service, _ip] = node_name |> Atom.to_string() |> String.split("@")
+    service
   end
 end
