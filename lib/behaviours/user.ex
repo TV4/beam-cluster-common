756a649edbc6bf6891774114e923aba6a339a2c6
Phuong Van <phuong.van@erlang-solutions.com>
2024-02-09 15:47:18 +0100

    Remove direct db operation

--- lib/behaviours/user.ex
+++ lib/behaviours/user.ex
@@ -18,19 +18,6 @@ defmodule Behaviours.User do
 
   @callback fetch(Types.User.user_query()) :: {:ok, Types.User.user()} | {:error, :not_found}
 
-  @callback fetch_user_id_by_update_password_token(String.t()) :: {:ok, integer()} | nil
-
-  @callback insert(Types.User.parsed_input_user_data()) ::
-              {:ok, Types.User.user()} | {:error, Ecto.Changeset.t()}
-
-  @callback insert_or_update(Types.User.parsed_input_user_data()) ::
-              {:ok, Types.User.user()} | {:error, Ecto.Changeset.t()}
-
-  @callback update(Types.User.user(), Types.User.parsed_input_user_data()) ::
-              {:ok, Types.User.user()} | {:error, Ecto.Changeset.t()}
-
-  @callback delete(Types.User.user()) :: {:ok, Types.User.user()} | {:error, Ecto.Changeset.t()}
-
   # parse user calls
   @callback to_database_user_params(Types.User.user_input_params()) ::
               {:ok, Structs.User.parsed_user_data()} | {:error, term()}
