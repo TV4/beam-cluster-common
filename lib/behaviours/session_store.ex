934b6711e0bb09350265541d6624496975ffe948
Phuong Van <phuong.van@erlang-solutions.com>
2024-02-14 17:01:15 +0100

    add badrpc as return avalue in session store spec

--- lib/behaviours/session_store.ex
+++ lib/behaviours/session_store.ex
@@ -1,24 +1,29 @@
 defmodule Behaviours.SessionStore do
   alias Structs.Session
-  @callback all(user_id :: String.t()) :: map() | no_return
+
+  # Subject to change: Since we call session store in account, auth,.., with RPC call, add RPC error as one of the possible return type spec
+  # May remove, update if we use different strategy
+  @type badrpc_error :: {:badrpc, term()}
+
+  @callback all(user_id :: String.t()) :: map() | badrpc_error()
   @callback delete(user_id :: String.t(), session_id :: String.t()) ::
-              :ok | :error
-  @callback delete_all(user_id :: String.t()) :: [Session.t()]
+              :ok | :error | badrpc_error()
+  @callback delete_all(user_id :: String.t()) :: [Session.t()] | badrpc_error()
 
   @callback delete_other(user_id :: String.t(), session_id :: String.t()) ::
-              [Session.t()]
+              [Session.t()] | badrpc_error()
 
   @callback get(session_id :: String.t()) ::
-              %Session{} | nil | {:error, any()}
+              %Session{} | nil | {:error, any()} | badrpc_error()
 
   @callback store(session :: Session.t()) ::
-              String.t() | :error
+              String.t() | :error | badrpc_error()
 
   @callback update_session_id(
               user_id :: String.t(),
               session_id :: String.t()
             ) ::
-              {:ok, String.t()} | :error
+              {:ok, String.t()} | :error | badrpc_error()
 
-  @callback session_ids_for_user(user_id :: String.t()) :: list()
+  @callback session_ids_for_user(user_id :: String.t()) :: list() | badrpc_error()
 end
